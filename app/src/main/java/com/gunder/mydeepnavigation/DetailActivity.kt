1c2d86f9e6e3bcd4b3c321c5e1b26090a2bfba2e
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-05 12:44:33 +0700

    add lines code of DetailActivity

--- app/src/main/java/com/gunder/mydeepnavigation/DetailActivity.kt
+++ app/src/main/java/com/gunder/mydeepnavigation/DetailActivity.kt
@@ -2,10 +2,26 @@ package com.gunder.mydeepnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gunder.mydeepnavigation.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    //    binding
+    private lateinit var binding: ActivityDetailBinding
+
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MASSAGE = "extra_massage"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MASSAGE)
+
+        binding.tvTitle.text = title
+        binding.tvMassage.text = message
     }
 }
